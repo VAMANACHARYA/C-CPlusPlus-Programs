98548025ccfcb312ef35eb310323db543cad0173
VAMAN ACHARYA <17621846+VAMANACHARYA@users.noreply.github.com>
2018-08-09 00:43:43 +0530

    Update Template.h
    Moving all methods to header file

--- Template/Template.h
+++ Template/Template.h
@@ -1,7 +1,23 @@
+#include <iostream>
+using namespace std;
+
 template <class T>
 T GetMax(T a, T b)
 {
 	T result;
 	result = (a > b) ? a : b;
 	return (result);
+}
+
+template <class S>
+S Swap(S a, S b)
+{
+	a = a + b;
+	b = a - b;
+	a = a - b;
+
+	cout << "Value of a after swap is: " << a << endl;
+	cout << "Value of b after swap is: " << b<< endl;
+
+	return 0;
 }
