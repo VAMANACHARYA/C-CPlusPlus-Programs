434a6ba2f2af0f214af2684c1bedf20d23f26cbc
VAMAN ACHARYA <vaman.acharya001@gmail.com>
2018-08-09 00:56:21 +0530

    Moving methods to header

--- Template/Template.cpp
+++ Template/Template.cpp
@@ -1,7 +1,4 @@
-#include <iostream>
 #include "Template.h"
-
-using namespace std;
 
 int main()
 {
@@ -9,12 +6,25 @@ int main()
 	cin >> fst;
 	cin >> scnd;
 	int x = GetMax(fst, scnd);
-	
+
 	int fst1, scnd2;
 	cin >> fst1;
 	cin >> scnd2;
 	int y = GetMax(fst1, scnd2);
-	cout << "Maximum of fst and scnd is: "<< x << endl;
+
+	cout << "Maximum of fst and scnd is: " << x << endl;
 	cout << "Maximum of fst1 and scnd2 is :" << y << endl;
+
+	int iValA, iValB;
+
+	cin >> iValA;
+	cout << "Value of a before swap: " << iValA << endl;
+
+	cin >> iValB;
+	cout << "Value of b before swap: " << iValB << endl;
+
+	Swap(iValA, iValB);
+
+	_getch();
 	return 0;
 }
