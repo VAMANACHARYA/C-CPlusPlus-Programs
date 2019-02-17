33cbd7b01952898b84b83d53bcbab46ac33d2a7a
VAMAN ACHARYA <vaman.acharya001@gmail.com>
2019-02-18 00:40:11 +0530

    Quick sort

---
+++ TwoFIleCompile/Win32Project1/QuickSort.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+
+void swap(int* a, int* b)
+{
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
+
+int partition(int arr[], int low, int high)
+{
+	int pivot = arr[high];    // pivot 
+	int i = (low - 1);  // Index of smaller element 
+
+	for (int j = low; j <= high - 1; j++)
+	{
+		// If current element is smaller than or 
+		// equal to pivot 
+		if (arr[j] <= pivot)
+		{
+			i++;    // increment index of smaller element 
+			swap(&arr[i], &arr[j]);
+		}
+	}
+	swap(&arr[i + 1], &arr[high]);
+	return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high)
+{
+	if (low < high)
+	{
+		/* pi is partitioning index, arr[p] is now
+		at right place */
+		int pi = partition(arr, low, high);
+
+		// Separately sort elements before 
+		// partition and after partition 
+		quickSort(arr, low, pi - 1);
+		quickSort(arr, pi + 1, high);
+	}
+}
+
+void printArray(int arr[], int size)
+{
+	int i;
+	for (i = 0; i < size; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
+}
+void main()
+{
+	int arr[3] = {};
+	for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+		cin >> arr[i];
+
+	quickSort(arr, 0, (sizeof(arr) / sizeof(arr[0])) - 1);
+	printf("Sorted array: ");
+	printArray(arr, sizeof(arr) / sizeof(arr[0]));
+	_getch();
+}
