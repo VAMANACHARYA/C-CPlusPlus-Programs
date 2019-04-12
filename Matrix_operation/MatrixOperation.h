b8c090f64e683870fc9190fc617016573a178a6d
Vaman Acharya <vaman.acharya001@gmail.com>
2019-04-12 15:47:17 +0530

    Updates to continue/stop depending on user input

--- Matrix_operation/MatrixOperation.h
+++ Matrix_operation/MatrixOperation.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <conio.h>
+#include <string>
 using namespace std;
 
 class MatrixOperations
--- Matrix_operation/main.cpp
+++ Matrix_operation/main.cpp
@@ -2,42 +2,63 @@
 
 void main()
 {
-	int iRowA, iColA, iRowB, iColB;
-	cout << "Enter Row for ArrA" << endl;
-	cin >> iRowA;
-	cout << "Enter Column for ArrA" << endl;
-	cin >> iColA;
-	cout << "Enter Row for ArrB" << endl;
-	cin >> iRowB;
-	cout << "Enter column for ArrB" << endl;
-	cin >> iColB;
-	vector <vector <int>> iResult, iArr1, iArr2,IdentMat;
-	if (iRowA == iRowB && iColA == iColB)
+	bool QuitLoop = true;
+	while(QuitLoop)
 	{
-		MatrixOperations MatrixA(iRowA, iColA, iRowB, iColB);
-		cout << "Enter Elements of iArr1" << endl;
-		iArr1 = MatrixA.FillVector(iRowA, iColA);
-		cout << "Elements of iArr1" << endl;
-		MatrixA.showResults(iRowA,iColA, iArr1);
-		MatrixOperations MatrixB(iRowA, iColA, iRowB, iColB);
-		cout << "Enter Elements of iArr1" << endl;
-		iArr2 = MatrixB.FillVector(iRowB, iColB);
-		cout << "Elements of iArr2" << endl;
-		MatrixB.showResults(iRowB, iColB, iArr2);
-		MatrixOperations Result;
-		iResult = Result.MatrixAddition(iRowA, iColA, iArr1, iArr2);
-		cout << "Sum of Matrices is:" << endl;
-		Result.showResults(iRowA, iColA, iResult);
-		if (iRowA == iColA && iRowB == iColB) 
+		int iRowA, iColA, iRowB, iColB;
+		cout << "Enter Row for ArrA" << endl;
+		cin >> iRowA;
+		cout << "Enter Column for ArrA" << endl;
+		cin >> iColA;
+		cout << "Enter Row for ArrB" << endl;
+		cin >> iRowB;
+		cout << "Enter column for ArrB" << endl;
+		cin >> iColB;
+		vector <vector <int>> iResult, iArr1, iArr2, IdentMat;
+		if (!iRowA && !iColA && !iRowB && !iColB)
+			cout << "Enter Row and Columns greater than zero" << endl;
+		else
 		{
-			MatrixOperations iIdntyMatrix;
-			IdentMat = iIdntyMatrix.CreateIdentityMatrix(iRowA, iColA);
-			cout << "Identity Matrices is:" << endl;
-			iIdntyMatrix.showResults(iRowA, iColA, IdentMat);
+			if (iRowA == iRowB && iColA == iColB)
+			{
+				MatrixOperations MatrixA(iRowA, iColA, iRowB, iColB);
+				cout << "Enter Elements of iArrA" << endl;
+				iArr1 = MatrixA.FillVector(iRowA, iColA);
+				cout << "Elements of iArrA" << endl;
+				MatrixA.showResults(iRowA, iColA, iArr1);
+				MatrixOperations MatrixB(iRowA, iColA, iRowB, iColB);
+				cout << "Enter Elements of iArrB" << endl;
+				iArr2 = MatrixB.FillVector(iRowB, iColB);
+				cout << "Elements of iArrB" << endl;
+				MatrixB.showResults(iRowB, iColB, iArr2);
+				MatrixOperations Result;
+				iResult = Result.MatrixAddition(iRowA, iColA, iArr1, iArr2);
+				cout << "Sum of Matrices is:" << endl;
+				Result.showResults(iRowA, iColA, iResult);
+				if (iRowA == iColA && iRowB == iColB)
+				{
+					MatrixOperations iIdntyMatrix;
+					IdentMat = iIdntyMatrix.CreateIdentityMatrix(iRowA, iColA);
+					cout << "Identity Matrices is:" << endl;
+					iIdntyMatrix.showResults(iRowA, iColA, IdentMat);
+				}
+			}
+			else
+				cout << "Row column mismatch. Exit and reenter" << endl;
 		}
+		cout << "Do you want to quit?" << endl;
+		string sUserInput;
+		cin >> sUserInput;
+		for (int k = 0;k < sUserInput.length();k++)
+		{
+			sUserInput[k] = toupper(sUserInput[k]);
+		}
+		//transform(sUserInput.begin(), sUserInput.end(), sUserInput.begin(), ::toupper);
+		if (sUserInput == "NO")
+			QuitLoop = true;
+		else
+			QuitLoop = false;
 	}
-	else
-		cout << "Row column mismatch. Exit and reenter" << endl;
 	_getch();
 	return;
 }
